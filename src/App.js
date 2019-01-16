f2740af6d9d9d7b1781ab413413b542a8f4b2902
jpancho <jpancho@sfu.ca>
2019-01-16 15:56:03 -0800

    Add navbar components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router , Route, Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css"
 import './App.css';
+
 import ListTodo from "./components/list.component";
 import EditTodo from "./components/edit.component";
 import CreateTodo from "./components/create.component";
@@ -11,7 +12,19 @@ class App extends Component {
     return (
       <Router>
         <div className="container">
-          <h2>Todo App</h2>
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/" className="navbar-brand">Todo App</Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todo</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
           <Route path="/" exact component={ListTodo} />
           <Route path="/edit/:id" component={EditTodo} />
           <Route path="/create" component={CreateTodo} />
